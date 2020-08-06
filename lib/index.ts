ee9f545b97a11e3fc2752c459e971a71fe76664c
Steve Thompson <writetome51@gmail.com>
2020-08-05 19:40:48 -0600

    Refactored code for clarity

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import { errorIfIndexNotValid } from 'error-if-index-not-valid';
 import { getAndRemoveByIndex } from '@writetome51/array-get-and-remove-by-index';
-import { getCopy } from '@writetome51/array-get-copy';
+import { getArrayCopy } from '@writetome51/get-array-copy';
 import { getInNumericOrder } from '@writetome51/get-in-numeric-order';
 import { notEmpty } from '@writetome51/is-empty-not-empty';
 import { removeByIndex } from '@writetome51/array-remove-by-index';
@@ -13,49 +13,49 @@ import { setArray } from '@writetome51/set-array';
 // The items are returned in ascending index-order: i.e, item with index 0 appears first.
 
 export function getAndRemoveByIndexes(indexes: number[], array): any[] {
-	let indxs = getCopy(indexes); // make copy to preserve original.
-	convertToPositives_placeInOrder_andRemoveDuplicates(indxs);
-	let removedItems = _getAndRemoveByIndexes(indxs, array);
-	// Return all items in ascending index-order:
-	return removedItems.reverse();
+	let indxs = getArrayCopy(indexes); // preserving original.
+	validateIndexes(indxs, array);
+
+	return __getAndRemoveByIndexes(indxs, array);
 
 
-	function convertToPositives_placeInOrder_andRemoveDuplicates(indexes) {
-		convertNegativeIndexesToPositives(indexes);
-		// algorithm only works if they're in descending order:
-		placeInDescendingOrder(indexes);
+	function validateIndexes(indexes, array) {
+		convertNegativeIndexesToPositives(indexes, array);
 		removeDuplicates(indexes); // Just in case.
+
+
+		function convertNegativeIndexesToPositives(indexes, array) {
+			for (let i = 0; i < indexes.length; ++i) {
+				errorIfIndexNotValid(indexes[i], array.length);
+				if (indexes[i] < 0) indexes[i] = array.length + indexes[i];
+			}
+		}
 	}
 
 
-	function _getAndRemoveByIndexes(indexes, array) {
+	function __getAndRemoveByIndexes(indexes, array) {
+		// algorithm only works if they're in descending order:
+		placeInDescendingOrder(indexes);
+
 		let removedItems = [];
 		while (notEmpty(indexes)) {
-			removeItem_and_addToRemovedItems(indexes[0]);
+			removeItem_andAddTo_removedItems(indexes[0]);
 			removeByIndex(0, indexes);
 		}
-		return removedItems;
+		// Return all items in ascending index-order:
+		return removedItems.reverse();
 
 
-		function removeItem_and_addToRemovedItems(index) {
+		function placeInDescendingOrder(indexes) {
+			let ordered = getInNumericOrder(indexes).reverse();
+			setArray(indexes, ordered);
+		}
+
+
+		function removeItem_andAddTo_removedItems(index) {
 			let item = getAndRemoveByIndex(index, array);
 			removedItems.push(item);
 		}
 	}
-
-
-	function convertNegativeIndexesToPositives(indexes) {
-		for (let i = 0; i < indexes.length; ++i) {
-			errorIfIndexNotValid(indexes[i], array.length);
-			if (indexes[i] < 0) indexes[i] = array.length + indexes[i];
-		}
-	}
-
-
-	function placeInDescendingOrder(indexes) {
-		let ordered = getInNumericOrder(indexes).reverse();
-		setArray(indexes, ordered);
-	}
-
 
 }

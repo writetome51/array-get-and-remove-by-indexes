bfd8fd4daa53272c98f3ade191d0a2453ca848c0
Steve Thompson <writetome51@gmail.com>
2020-09-06 17:11:16 -0600

    Changed implementation.  Function behaves the same

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
-import {getAndRemoveByIndex} from '@writetome51/array-get-and-remove-by-index';
-import {getArrayCopy} from '@writetome51/get-array-copy';
-import {orderNumerically} from '@writetome51/order-numerically';
+import {removeByIndexes} from '@writetome51/array-remove-by-indexes';
 import reverse from '@arr/reverse';
 
 
@@ -8,27 +6,11 @@ import reverse from '@arr/reverse';
 // The items are returned in ascending index-order: i.e, item with index 0 appears first.
 
 export function getAndRemoveByIndexes(indexes, array) {
-	indexes = getArrayCopy(indexes);
+	let removedItems = [];
 
-	return __getAndRemoveByIndexes(indexes, array);
+	// Adds items to removedItems in descending index order:
+	removeByIndexes(indexes, array, (item) => removedItems.push(item));
 
-
-	function __getAndRemoveByIndexes(indexes, array) {
-		orderNumerically(indexes);
-
-		let removedItems = [], i = indexes.length;
-		// algorithm only works if they're removed in descending order:
-		while (--i > -1) removeItem_andAddTo_removedItems(indexes[i]);
-
-		// Return all items in ascending index-order:
-		return reverse(removedItems);
-
-
-		function removeItem_andAddTo_removedItems(index) {
-			if (index < 0) throw new Error('Negative indexes are not allowed in this function');
-			let item = getAndRemoveByIndex(index, array);
-			removedItems.push(item);
-		}
-	}
-
+	// Return items in ascending index-order:
+	return reverse(removedItems);
 }
